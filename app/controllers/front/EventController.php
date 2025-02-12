bafdb56a2851f33a99e95c5b84d42670e73cfec8
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-02-12 21:58:02 +0100

    CONTROLLER: added method of the event page in the right class

--- app/controllers/front/EventController.php
+++ app/controllers/front/EventController.php
@@ -3,8 +3,21 @@
 namespace Controller_front;
 
 use Core\Controller;
+use Model\Category;
 
 class EventController extends Controller{
 
-    
+    public function store() {
+        if ($_SERVER["REQUEST_METHOD"] === "POST"){
+            
+        }
+    }
+
+    public function event(){
+        $categoryModel = new Category();
+        $categories = $categoryModel->getAllCategory();
+        $this->view("front/create_event", [
+            'categories' => $categories
+        ]);
+    }
 }
