f16fa743739703be84375a7d28ee3a73f5ece432
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-02-12 15:28:29 +0100

    GLOBAL: small change

--- app/controllers/front/UserController.php
+++ app/controllers/front/UserController.php
@@ -3,10 +3,23 @@
 namespace Controller_front;
 
 use Core\Controller;
+use Model\Event;
+use Model\Promo;
 
 class UserController extends Controller {
-    public function index (){
-        $this->view("front/userDash");
+    public function index() {
+        $eventModel = new Event();
+        $promoModel = new Promo();
+
+        $userId = $_SESSION['user']["id"] ?? null;
+        
+        $events = $eventModel->getAllEvent();
+        $promos = $promoModel->getAllPromosWithEvents(); // Get all promos with event details
+        
+        $this->view("front/userDash", [
+            'events' => $events,
+            'promos' => $promos
+        ]);
     }
 
     public function event(){
