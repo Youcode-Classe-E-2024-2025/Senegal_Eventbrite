fc4a937f00e253c7c09668d06804f986ad0e5587
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-02-12 15:47:10 +0100

    CLASS: promo updated

--- app/models/Promo.php
+++ app/models/Promo.php
@@ -9,15 +9,17 @@ class Promo extends Model {
         return $this->insert("promo", $data);
     }
     
-    public function getAllPromosWithEvents() {
-        $query = "SELECT p.*, e.title as event_title 
-                 FROM promo p 
-                 JOIN events e ON p.event_id = e.id 
-                 ORDER BY p.id DESC";
-        $stmt = $this->db->prepare($query);
-        $stmt->execute();
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-    }
+    public function getAllPromosWithEvents($userId)
+{
+    $query = "SELECT p.*, e.title as event_title 
+              FROM promo p 
+              JOIN events e ON p.event_id = e.id 
+              WHERE e.organizer_id = ?
+              ORDER BY p.id DESC";
+    $stmt = $this->db->prepare($query);
+    $stmt->execute([$userId]);
+    return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+}
     
     public function deletePromo($id) {
         return $this->delete('promo', $id);
